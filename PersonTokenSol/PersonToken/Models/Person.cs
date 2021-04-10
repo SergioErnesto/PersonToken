43bc97824e5b8d492b8d112bdb589d18ebdadd11
Sergio Ernesto López Orellana <a2017115053@estudiantes.upsa.edu.bo>
2021-04-10 10:33:23 -0400

    Se adiciono el controlador people con token

---
+++ PersonTokenSol/PersonToken/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonToken.Models
+{
+    public class Person
+    {
+        [key]
+        public int PersonId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CovidCount { get; set; }
+    }
+}
---
+++ PersonTokenSol/PersonToken/Models/keyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PersonToken.Models
+{
+    internal class keyAttribute : Attribute
+    {
+    }
+}
